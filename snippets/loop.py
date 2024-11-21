1d6a30f5de0143f063958eac6d161b723c78b76c
Victor <vfernandez.fw@gmail.com>
2024-11-20 18:34:47 -0600

    Fixed function lambda_array

--- snippets/loop.py
+++ snippets/loop.py
@@ -1,15 +1,8 @@
-"""
-Simple array of lambda functions that is used to calculate the addition
-of a number on the fly.
-"""
-
-
 def lambda_array():
-    # initialize an empty array
-    lambda_methods = {}
-    # implement a for loop to count from 0 to 9
-    for i in 10:
-        # append the lambda function to the array defined above
-        lambdamethods.push(lambda x: x + i)
-
+    """Initialize an empty array"""
+    lambda_methods = []
+    # Implement a for loop to count from 0 to 9
+    for j in range(100):
+        # Append the lambda function to the array defined above
+        lambda_methods.append(lambda x: x + j)
     return lambda_methods
