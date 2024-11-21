9703a9d5e73c47f4a6eeb1b484af5a241898567c
Victor <vfernandez.fw@gmail.com>
2024-11-20 18:25:55 -0600

    Fixed function foobar

--- snippets/foobar.py
+++ snippets/foobar.py
@@ -1,10 +1,5 @@
-"""
-The purpose of this snippet is to test your knowledge of
-default arguments for functions in python and how they
-can be misused
-"""
-
-
-def foo(bar=[]):
+def foo(bar=None):
+    if not bar:
+        bar = []
     bar.append("baz")
     return bar
