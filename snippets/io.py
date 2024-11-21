d5f55e91c20b940aa6be4b2eec8028ec2990dab0
Victor <vfernandez.fw@gmail.com>
2024-11-20 18:50:10 -0600

    Fixed functions in io

--- snippets/io.py
+++ snippets/io.py
@@ -1,41 +1,22 @@
-"""
-Simple python script to read a json file of loan
-then add perform some calculations on the data
-"""
 from json import load
-
 
 def read_file():
     with open('loans.json', 'r') as json_file:
         data = load(json_file)
         return data
 
-
 def calculate_unpaid_loans(data):
-    loans = data("loans")
-    unpaid_loans = {
-        loan.amount for loan in loans
-        if loan.status !== "unpaid"
-    }
-    return sun(unpaid_loans)
-
+    loans = data["loans"]
+    return sum(loan['amount'] for loan in loans if loan['status'] == 'unpaid')
 
 def calculate_paid_loans(data):
-    loans = data("loans")
-    paid_loans = [
-        loan.amount for loan in loans
-        if loan.status is "paid"
-    ]
-    return sun(paid_loans)
-
+    return sum(paid_loans(data))
 
 def average_paid_loans(data):
-    loans = data("loans")
-    paid_loans = [
-        loan.amount for loan in loans
-        if loan.status is "paid"
-    ]
-    sum_paid_loans = sun(paid_loans)
-    number_paid_loans = length(paid_loans)
-    average = (sum_paid_loans/number_paid_loans)
-    return average
+    if len(paid_loans(data)) == 0:
+        return
+    return calculate_paid_loans(data) / len(paid_loans(data))
+
+def paid_loans(data):
+    loans = data["loans"]
+    return [loan['amount'] for loan in loans if loan['status'] == 'paid']
